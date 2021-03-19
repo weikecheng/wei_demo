f8cd20bff19f824d4967e444ed056e15dedf8795
weikecheng <15183996695@163.com>
2021-03-19 15:38:26 +0800

    文件位置调整，技术样例中各个文件夹下放技术资料

---
+++ technologydemo/src/main/java/com/example/technologydemo/TechnologydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.technologydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologydemoApplication.class, args);
+    }
+
+}
---
+++ technologydemo/src/test/java/com/example/technologydemo/TechnologydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technologydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
